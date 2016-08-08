b77c829a644c3ed465f558f651781e2d0a1e639b
rafstraumur <simon.dirmeier@web.de>
2016-08-08 23:50:30 +0200

    changed email

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 from setuptools import setup
 
-
 def readme():
     with open('README.rst') as f:
         return f.read()
-
 
 setup(name='datatable',
       version='0.1',
@@ -13,11 +11,25 @@ setup(name='datatable',
       keywords='datatable python',
       url='http://github.com/rafstraumur/datatable',
       author='Simon Dirmeier',
-      author_email='datatable@simon-dirmeier.net',
+      author_email='simon.dirmeier@gmx.de',
       keywords = ['table'],
       packages=['datatable'],
        install_requires=[
           'scipy',
           'numpy'
-      ],
+      ]
+)
+
+from distutils.core import setup
+setup(
+  name = 'mypackage',
+  packages = ['mypackage'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Peter Downs',
+  author_email = 'peterldowns@gmail.com',
+  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
 )
