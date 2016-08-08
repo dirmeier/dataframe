6c10a31b0a81260ea060ac0218202745cf7b902d
rafstraumur <simon.dirmeier@web.de>
2016-08-08 23:59:24 +0200

    changed setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ def readme():
     with open('README.rst') as f:
         return f.read()
 
-setup(name='datatable',
+setup(name='dataframe',
       version='0.1',
       description='Efficient data-frame implementation in python',
       long_description=readme(), 
